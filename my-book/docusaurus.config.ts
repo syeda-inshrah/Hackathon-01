648bf2f0e53fe57410f676affdb4683086d0607b
Syeda-inshrah <161240095+syeda-inshrah@users.noreply.github.com>
2025-12-13 20:38:04 +0500

    Fix broken link in footer: update /docs/intro to /docs/introduction

--- my-book/docusaurus.config.ts
+++ my-book/docusaurus.config.ts
@@ -103,7 +103,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/introduction',
             },
           ],
         },
